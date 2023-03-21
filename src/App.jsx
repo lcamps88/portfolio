9db3cc3ddfc3c145b2cef20f273634dcaeb4e90f
Lisandra <lisandra.camps@gmail.com>
2023-03-20 22:03:45 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Sidenav from './components/SideNav'
+
+const App = () => {
+  return (
+    <div>
+      <Sidenav />
+    </div>
+  )
+}
+
+export default App
